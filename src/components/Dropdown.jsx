9959b7ed14b8e546579940075732a8bc4a884f7c
Jonathan Wylie <jwwylie1@gmail.com>
2025-07-17 23:33:29 -0400

    undo stupid

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -17,7 +17,7 @@ function Dropdown() {
 
   return (
     <>
-    <h1 className="text-xl font-bold mb-4">Telemetry Data</h1>
+    <h1 className="text-xl font-bold mb-4">Tewemetwy Data</h1>
         {telemetry ? (
             telemetry.map((entry, index) => (
             <div
@@ -25,7 +25,7 @@ function Dropdown() {
                 className="mb-3 p-2 border border-gray-300 rounded bg-gray-50"
             >
                 {/* Customize fields you want to show here */}
-                <p><strong>Session Ky:</strong> {entry.session_key}</p>
+                <p><strong>Session Key:</strong> {entry.session_key}</p>
                 <p><strong>Driver Number:</strong> {entry.driver_number}</p>
                 <p><strong>Speed:</strong> {entry.speed} km/h</p>
                 <p><strong>Throttle:</strong> {entry.throttle}%</p>
