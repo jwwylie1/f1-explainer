1cd099c01d876ca1a34aa8a71536de2b52842d33
jwwylie2 <jonathan.wylie@kpit.com>
2025-07-22 16:27:23 -0400

    mapped Bahrain-Canada

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,21 +1,11 @@
 import React, { useEffect, useState, useRef } from "react";
 
-const SCALE = 0.0000432
-const INIT_X = 0.353;
-const INIT_Y = 0.110;
-const ANGLE = 225;
+const SCALE = 0.000038;
+const ERROR_X = 0.15;
+const ERROR_Y = 0.1;
+const ANGLE = 200; // Increase = counterclockwise
+const FLIP = [-1,1]
 const SPEED_MULT = 1;
-
-function rotate(x, y, scale, rotation, flips) {
-  // Convert rotation from degrees to radians
-  const radians = (rotation * Math.PI) / 90;
-
-  // Calculate the new x and y values
-  const newX = x * Math.cos(radians) - y * Math.sin(radians);
-  const newY = x * Math.sin(radians) + y * Math.cos(radians);
-
-  return { x: scale * flips[0] * newX, y: scale * flips[1] * newY };
-}
 
 function Canvas({ race }) {
   const canvasRef = useRef(null);
@@ -39,20 +29,35 @@ function Canvas({ race }) {
   const startTime1 = useRef(null);
   const startTime2 = useRef(null);
 
+  function rotate(x, y, scale, deg, flips) {
+    // Convert rotation from degrees to radians
+    const radians = (deg * Math.PI) / 180;
+    const Xs = x * Math.cos(radians) - y * Math.sin(radians);
+    const Ys = x * Math.sin(radians) + y * Math.cos(radians);
+
+    const Xw = Xs * flips[0] * scale;
+    const Yw = Ys * flips[1] * scale;
+
+    const newX = Xw + canvasSize[0]/2 + ERROR_X*canvasSize[0]
+    const newY = Yw + canvasSize[1]/2 + ERROR_Y*canvasSize[1]
+
+    return { x: newX, y: newY };
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       const [car1LocRes, car2LocRes, car1DataRes, car2DataRes] = await Promise.all([
         fetch(
-          "https://api.openf1.org/v1/location?session_key=9126&driver_number=81&date%3E2023-07-09T14:34:16.764000+00:00&date%3C2023-07-09T14:35:49.527000+00:00"
+          "https://api.openf1.org/v1/location?session_key=9118&driver_number=81&date%3E2023-07-02T13:38:02.735000+00:00&date%3C2023-07-02T13:40:02.735000+00:00"
         ),
         fetch(
-          "https://api.openf1.org/v1/location?session_key=9693&driver_number=27&date%3E2025-03-16T04:57:43.997000+00:00&date%3C2025-03-16T04:59:17.156000+00:00"
+          "https://api.openf1.org/v1/location?session_key=9118&driver_number=27&date%3E2023-07-02T13:38:42.735000+00:00&date%3C2023-07-02T13:40:42.735000+00:00"
         ),
         fetch (
-          "https://api.openf1.org/v1/car_data?session_key=9693&driver_number=81&date%3E2025-03-16T04:56:52.676000+00:00&date%3C2025-03-16T04:58:22.881000+00:00",
+          "https://api.openf1.org/v1/car_data?session_key=9998&driver_number=81&date%3E2025-03-23T07:36:38.944000+00:00&date%3C2025-03-23T07:38:16.841000+00:00",
         ),
         fetch(
-          "https://api.openf1.org/v1/car_data?session_key=9693&driver_number=27&date%3E2025-03-16T04:57:43.997000+00:00&date%3C2025-03-16T04:59:17.156000+00:00"
+          "https://api.openf1.org/v1/car_data?session_key=9998&driver_number=27&date%3E2025-03-23T07:39:16.476000+00:00&date%3C2025-03-23T07:40:54.062000+00:00"
         )
       ]);
       const [car1Location, car2Location, car1Data, car2Data] = await Promise.all([
@@ -61,6 +66,7 @@ function Canvas({ race }) {
         car1DataRes.json(),
         car2DataRes.json(),
       ]);
+
       setCar1Location(car1Location);
       setCar2Location(car2Location);
       setCar1Data(car1Data);
@@ -74,7 +80,8 @@ function Canvas({ race }) {
     if (car1Location && car2Location && race) {
       const img = new Image();
       console.log(race)
-      img.src = `./src/assets/circuits/${race.name}.webp`;
+      //img.src = `./src/assets/circuits/${race.name}.webp`;
+      img.src = `./src/assets/circuits/Austria.webp`;
       img.onload = () => {
         const canvasWidth = window.innerWidth * 0.8; // 80% of the page width
         const aspectRatio = img.naturalWidth / img.naturalHeight;
@@ -106,8 +113,17 @@ function Canvas({ race }) {
         context.putImageData(pixels, 0, 0);
         context.lineWidth = 3;
 
-        setDriver1({x: INIT_X*canvasWidth, y: INIT_Y*canvasHeight})
-        setDriver2({x: INIT_X*canvasWidth, y: INIT_Y*canvasHeight})
+
+        context.fillStyle = 'blue'
+        context.beginPath();
+        context.arc(canvasWidth/2, canvasHeight/2, 10, 0, 2 * Math.PI); // x, y, radius, startAngle, endAngle
+        context.fill();
+
+        context.fillStyle = 'red'
+        context.beginPath();
+        const newC = rotate(0, 0, SCALE, ANGLE, FLIP)
+        context.arc(newC.x, newC.y, 10, 0, 2 * Math.PI); // x, y, radius, startAngle, endAngle
+        context.fill();
 
 
         if (!startTime1.current) {
@@ -136,42 +152,41 @@ function Canvas({ race }) {
 
     if (!ctx || (!car1Available && !car2Available)) return;
 
-    const locTime1 = new Date(car1Location[frameIndex1].date).getTime() - startTime1.current;
-    const locTime2 = new Date(car2Location[frameIndex2].date).getTime() - startTime2.current;
+    const locTime1 = new Date(car1Location[frameIndex1]?.date).getTime() - startTime1.current;
+    const locTime2 = new Date(car2Location[frameIndex2]?.date).getTime() - startTime2.current;
 
-    const dataTime1 = new Date(car1Data[dataIndex1].date).getTime() - startTime1.current;
-    const dataTime2 = new Date(car2Data[dataIndex2].date).getTime() - startTime2.current;
+    const dataTime1 = new Date(car1Data[dataIndex1]?.date).getTime() - startTime1.current;
+    const dataTime2 = new Date(car2Data[dataIndex2]?.date).getTime() - startTime2.current;
 
     if (car1Available && currentTime >= locTime1) {
       drawCar1(ctx, canvasRef.current);
-      //updateStatsCar1()
       setFrameIndex1(prev => prev+1);
+      console.log(car1Data[dataIndex1])
     }
     if (car2Available && currentTime >= locTime2) {
       drawCar2(ctx, canvasRef.current);
-      //updateStatsCar2()
       setFrameIndex2(prev => prev+1);
     }
 
     if (car1Available && currentTime >= dataTime1) {
       setCar1AvgData(prevData => ({
         ...prevData,
-        speed: prevData.speed + car1Data[dataIndex1].speed,
-        throttle: prevData.throttle + car1Data[dataIndex1].throttle,
-        brake: prevData.brake + car1Data[dataIndex1].brake,
-        gear: prevData.gear + car1Data[dataIndex1].n_gear,
-        rpm: prevData.rpm + car1Data[dataIndex1].rpm,
+        speed: prevData.speed + car1Data[dataIndex1]?.speed,
+        throttle: prevData.throttle + car1Data[dataIndex1]?.throttle,
+        brake: prevData.brake + car1Data[dataIndex1]?.brake,
+        gear: prevData.gear + car1Data[dataIndex1]?.n_gear,
+        rpm: prevData.rpm + car1Data[dataIndex1]?.rpm,
       }));
       setDataIndex1(prev => prev+1);
     }
     if (car2Available && currentTime >= dataTime2) {
       setCar2AvgData(prevData => ({
         ...prevData,
-        speed: prevData.speed + car2Data[dataIndex2].speed,
-        throttle: prevData.throttle + car2Data[dataIndex2].throttle,
-        brake: prevData.brake + car2Data[dataIndex2].brake,
-        gear: prevData.gear + car2Data[dataIndex2].n_gear,
-        rpm: prevData.rpm + car2Data[dataIndex2].rpm,
+        speed: prevData.speed + car2Data[dataIndex2]?.speed,
+        throttle: prevData.throttle + car2Data[dataIndex2]?.throttle,
+        brake: prevData.brake + car2Data[dataIndex2]?.brake,
+        gear: prevData.gear + car2Data[dataIndex2]?.n_gear,
+        rpm: prevData.rpm + car2Data[dataIndex2]?.rpm,
       }));
       setDataIndex2(prev => prev+1);
     }
@@ -182,21 +197,23 @@ function Canvas({ race }) {
 
     ctx.strokeStyle = "orange";
     const newCoords1 = rotate(
-      car1Location[frameIndex1 + 1].x - car1Location[frameIndex1].x,
-      car1Location[frameIndex1 + 1].y - car1Location[frameIndex1].y,
+      car1Location[frameIndex1 + 1].x,
+      car1Location[frameIndex1 + 1].y,
       SCALE*canvasSize[0],
       ANGLE,
-      [-1, 1]
+      FLIP
     );
     ctx.beginPath(); // Start a new path
     ctx.moveTo(driver1.x, driver1.y); // Move the "pen" to the starting point
-    ctx.lineTo(driver1.x + newCoords1.x, driver1.y + newCoords1.y); // Draw a line to the ending point
+    ctx.lineTo(newCoords1.x, newCoords1.y); // Draw a line to the ending point
     ctx.stroke(); // Actually draw the line (use ctx.fill() for filled lines)
     ctx.closePath(); // Close the path
-    setDriver1((prev) => ({
-      x: prev.x + newCoords1.x,
-      y: prev.y + newCoords1.y,
+    setDriver1(() => ({
+      x: newCoords1.x,
+      y: newCoords1.y,
     }));
+
+    console.log(newCoords1)
 
     setImage(canvas.toDataURL());
   };
@@ -205,20 +222,20 @@ function Canvas({ race }) {
 
     ctx.strokeStyle = "green"
     const newCoords2 = rotate(
-      car2Location[frameIndex2 + 1].x - car2Location[frameIndex2].x,
-      car2Location[frameIndex2 + 1].y - car2Location[frameIndex2].y,
+      car2Location[frameIndex2 + 1].x,
+      car2Location[frameIndex2 + 1].y,
       SCALE*canvasSize[0],
       ANGLE,
-      [-1, 1]
+      FLIP
     );
     ctx.beginPath(); // Start a new path
     ctx.moveTo(driver2.x, driver2.y); // Move the "pen" to the starting point
-    ctx.lineTo(driver2.x + newCoords2.x, driver2.y + newCoords2.y); // Draw a line to the ending point
+    ctx.lineTo(newCoords2.x, newCoords2.y); // Draw a line to the ending point
     ctx.stroke(); // Actually draw the line (use ctx.fill() for filled lines)
     ctx.closePath(); // Close the path
-    setDriver2((prev) => ({
-      x: prev.x + newCoords2.x,
-      y: prev.y + newCoords2.y,
+    setDriver2(() => ({
+      x: newCoords2.x,
+      y: newCoords2.y,
     }));
 
     setImage(canvas.toDataURL());
@@ -255,23 +272,23 @@ function Canvas({ race }) {
 
           <tr>
             <td>Oscar PIASTRI</td>
-            <td style={{ opacity: car1Data?.[dataIndex1].speed / 200}}>
-              {car1Data?.[dataIndex1].speed} / {Math.floor(car1Data?.[dataIndex1].speed/1.609)}
+            <td style={{ opacity: car1Data?.[dataIndex1]?.speed / 200}}>
+              {car1Data?.[dataIndex1]?.speed} / {Math.floor(car1Data?.[dataIndex1]?.speed/1.609)}
             </td>
-            <td style={{ opacity: car1Data?.[dataIndex1].speed / 200}}>
+            <td style={{ opacity: car1Data?.[dataIndex1]?.speed / 200}}>
               {(car1AvgData.speed/dataIndex1).toFixed(2)}
             </td>
 
             <td style={{ color: `rgb(17, 
-              ${Math.floor((car1Data?.[dataIndex1].throttle / 100) * 255)}, 17)` }}>
-              {car1Data?.[dataIndex1].throttle}
+              ${Math.floor((car1Data?.[dataIndex1]?.throttle / 100) * 255)}, 17)` }}>
+              {car1Data?.[dataIndex1]?.throttle}
             </td>
             <td style={{ color: `rgb(17, 
               ${Math.floor((car1AvgData.throttle/dataIndex1 / 100) * 255)}, 17)` }}>
               {(car1AvgData.throttle/dataIndex1).toFixed(2)}
             </td>
 
-            <td style={{ color: car1Data?.[dataIndex1].brake === 0 ? '#111' : 'red' }}>
+            <td style={{ color: car1Data?.[dataIndex1]?.brake === 0 ? '#111' : 'red' }}>
               BRAKE
             </td>
             <td style={{ color: `rgb(${Math.floor((car1AvgData.brake/dataIndex1 / 30) * 255)}, 
@@ -279,30 +296,30 @@ function Canvas({ race }) {
               {(car1AvgData.brake/dataIndex1).toFixed(2)}
             </td>
 
-            <td>{car1Data?.[dataIndex1].n_gear}</td>
+            <td>{car1Data?.[dataIndex1]?.n_gear}</td>
             <td>{(car1AvgData.gear/dataIndex1).toFixed(2)}</td>
-            {/*<td>{car1Data?.[dataIndex1].rpm}</td> */}
+            {/*<td>{car1Data?.[dataIndex1]?.rpm}</td> */}
           </tr>
 
           <tr>
             <td>Nico HULKENBERG</td>
-            <td style={{ opacity: car2Data?.[dataIndex2].speed / 200}}>
-              {car2Data?.[dataIndex2].speed} / {Math.floor(car2Data?.[dataIndex2].speed/1.609)}
+            <td style={{ opacity: car2Data?.[dataIndex2]?.speed / 200}}>
+              {car2Data?.[dataIndex2]?.speed} / {Math.floor(car2Data?.[dataIndex2]?.speed/1.609)}
             </td>
-            <td style={{ opacity: car2Data?.[dataIndex2].speed / 200}}>
+            <td style={{ opacity: car2Data?.[dataIndex2]?.speed / 200}}>
               {(car2AvgData.speed/dataIndex2).toFixed(2)}
             </td>
 
             <td style={{ color: `rgb(17, 
-              ${Math.floor((car2Data?.[dataIndex2].throttle / 100) * 255)}, 17)` }}>
-              {car2Data?.[dataIndex2].throttle}
+              ${Math.floor((car2Data?.[dataIndex2]?.throttle / 100) * 255)}, 17)` }}>
+              {car2Data?.[dataIndex2]?.throttle}
             </td>
             <td style={{ color: `rgb(17, 
               ${Math.floor((car2AvgData.throttle/dataIndex2 / 100) * 255)}, 17)` }}>
               {(car2AvgData.throttle/dataIndex2).toFixed(2)}
             </td>
 
-            <td style={{ color: car2Data?.[dataIndex2].brake === 0 ? '#111' : 'red' }}>
+            <td style={{ color: car2Data?.[dataIndex2]?.brake === 0 ? '#111' : 'red' }}>
               BRAKE
             </td>
             <td style={{ color: `rgb(${Math.floor((car2AvgData.brake/dataIndex2 / 30) * 255)}, 
@@ -310,7 +327,7 @@ function Canvas({ race }) {
               {(car2AvgData.brake/dataIndex2).toFixed(2)}
             </td>
 
-            <td>{car2Data?.[dataIndex2].n_gear}</td>
+            <td>{car2Data?.[dataIndex2]?.n_gear}</td>
             <td>{(car2AvgData.gear/dataIndex2).toFixed(2)}</td>
 {/*             <td>{currentTime.toString().slice(-6,-3)}:{currentTime.toString().slice(-3,-2)}</td>
  */}         </tr>
